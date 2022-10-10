ce20fd0843df568baf26e167b87a401d5ca3f204
Douxami <peredouxami@gmail.com>
2022-10-10 21:03:50 +0100

    modification titre app

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'My MDM SCOOP app'),
+      home: const MyHomePage(title: 'MDM SCOOP app'),
     );
   }
 }
