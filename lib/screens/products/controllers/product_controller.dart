39d5d4c9a92ff6080e2f40bdd641fd5af9fd8fe3
patson120 <patrickkennenl@gmail.com>
2022-11-20 09:50:21 +0100

    Ajout de deux formulaires

--- lib/screens/products/controllers/product_controller.dart
+++ lib/screens/products/controllers/product_controller.dart
@@ -1,6 +1,11 @@
-
 import 'package:get/get.dart';
 
-class ProductController extends GetxController{
-
+class ProductController extends GetxController {
+  int i = 0;
+  Future doing() async {
+    print("Refreshing...");
+    await Future.delayed(const Duration(seconds: 5,));
+    print(++i);
+    await Future.delayed(const Duration(seconds: 3,));
+  }
 }
