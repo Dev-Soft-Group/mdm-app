84346f1877bfa25d590007e23804fe9d7ff9cd6c
patson120 <patrickkennenl@gmail.com>
2022-12-17 07:15:33 +0100

    Ajout de méthode pour retrouver les produits et publication d'une entreprise

--- lib/core/app_constantes.dart
+++ lib/core/app_constantes.dart
@@ -3,8 +3,8 @@
 // ignore_for_file: constant_identifier_names
 
 class Constantes {
-  static const String API_URL = "http://192.168.220.1:8000/api/v1";
-  // static const String API_URL = "http://mdmscoops.pythonanywhere.com/api/v1";
+  // static const String API_URL = "http://192.168.220.1:8000/api/v1";
+  static const String API_URL = "http://mdmscoops.pythonanywhere.com/api/v1";
   // static const String API_URL = "http://10.228.47.156:8000/api/v1";
   static const String ACCESSTOKEN = "ACCESSTOKEN";
   static const String USERDATA = "USERDATA";
